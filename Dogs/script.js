b305101fff30452ae93aff7edf6b45d2a804ab50
Naira Yeghiazaryan <yeghiazaryannaira97@gmail.com>
2020-08-07 18:40:02 +0400

    in Dogs task added Loading....

--- Dogs/script.js
+++ Dogs/script.js
@@ -5,12 +5,15 @@ const wrapper = document.querySelector('.wrapper')
 const dogImg = document.querySelector('img')
 
 breedList.addEventListener("change", () => {
+  wrapper.innerText = "Loading.....";
   const url = `https://dog.ceo/api/breed/${breedList.value}/images/random`;
   doGet(url)
     .then((r) => {
+      wrapper.innerText = "";
       if (r.status === "success") {
         dogImg.classList.add("show-img");
         dogImg.src = r.message;
+        wrapper.append(dogImg);
       } else {
         wrapper.innerHTML = "Oops!!!!! Something has gone wrong";
       }
